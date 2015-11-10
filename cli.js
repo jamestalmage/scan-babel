54eb6a1e3101e185d931a111d412ca7750e10e0d
James Talmage <james@talmage.io>
2015-11-10 18:23:59 -0500

    refine return values from scan-for-presets / scan-for-options

--- cli.js
+++ cli.js
@@ -1,24 +1,11 @@
 #!/usr/bin/env node
 'use strict';
+
+// So we can invoke from scan-babel directory, when installed alongside babel
+if (process.cwd() === __dirname) {
+	process.chdir(require('path').join(__dirname, '../babel'));
+}
+
 require('./lib/babel-register');
 require('loud-rejection')();
 require('./')();
-
-/*
-var meow = require('meow');
-var cli = meow([
-	'Usage',
-	'  $ scan-babel [input]',
-	'',
-	'Options',
-	'  --foo  Lorem ipsum. [Default: false]',
-	'',
-	'Examples',
-	'  $ scan-babel',
-	'  unicorns & rainbows',
-	'  $ scan-babel ponies',
-	'  ponies & rainbows'
-]);
-
-console.log(scanBabel(cli.input[0] || 'unicorns'));
- */
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 // import assert from 'assert';
-import {withPlugins} from './lib/scan-for-options';
+// import {withPlugins} from './lib/scan-for-options';
 import scanForPresets from './lib/scan-for-presets';
 
 // works in 6.1.3
@@ -11,12 +11,3 @@ export default async function () {
 	console.log(JSON.stringify(await scanForPresets(), null, 4));
 }
 
-   /*
-function pluginName(val) {
-	let name = val;
-	if (Array.isArray(val)) {
-		name = val[0];
-	}
-	assert.strictEqual(typeof name, 'string', `plugin should be a string, or an Array with a string at index 0: ${JSON.stringify(val)}`);
-	return name;
-}  */
