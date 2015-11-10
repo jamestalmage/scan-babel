b158dcbaa63b00f3188a7b75e206378cacdd310e
James Talmage <james@talmage.io>
2015-11-10 02:53:53 -0500

    clearer output from scan-for-presets

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict';
 // import assert from 'assert';
 import {withPlugins} from './lib/scan-for-options';
+import scanForPresets from './lib/scan-for-presets';
+
+// works in 6.1.3
 
 export default async function () {
-	console.log(JSON.stringify(await withPlugins(), null, 4));
+	// console.log(JSON.stringify(await withPlugins(), null, 4));
+
+	console.log(JSON.stringify(await scanForPresets(), null, 4));
 }
 
    /*
